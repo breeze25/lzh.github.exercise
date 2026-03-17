023753ac54b99dd5d91e69139a9ba82dc110e95e
breeze25 <2574136423@qq.com>
2026-03-17 15:13:57 +0800

    4day

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,7 +1,9 @@
 package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -13,4 +15,26 @@ public interface DishFlavorMapper {
      */
 
     void insertBatch(List<DishFlavor> flavors);
+
+    /**
+     * 根据菜品id查询口味数据
+     * @param dishid
+     * @return
+     */
+    @Delete("delete from dish_flavor where dish_id = #{dishId}")
+    void deleteByDishId(Long dishid);
+
+    /**
+     * 根据菜品id批量删除口味数据
+     * @param dishIds
+     */
+    void deleteByDishIds(List<Long> dishIds);
+
+    /**
+     * 根据菜品id查询口味数据
+     * @param dishId
+     * @return
+     */
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> getByDishId(Long dishId);
 }
